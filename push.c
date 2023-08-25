47df92b6525c9c8001b3ab97cf323e8c0096a64b
Hoja Tesema <hojats@hotmail.com>
2023-08-25 10:42:23 -0700

    push.c

--- push.c
+++ push.c
@@ -1,47 +1,42 @@
 #include "monty.h"
 
 /**
- * f_push - pushes an element to the stack or queue
- * @head: double pointer to the head of the stack
- * @counter: line number of the instruction
+ * f_push - add node to the stack or queue
+ * @head: pointer to the head of the stack or queue
+ * @counter: line number in the input file
+ * 
+ * This function reads a string from the global state variable
+ * and converts it to an integer. Then it adds a node with that
+ * integer value to the stack or queue, depending on the mode.
  */
-
 void f_push(stack_t **head, unsigned int counter)
 {
-	char *arg;
-	int n, j = 0, flag = 0;
+    int n;
+    char *endptr;
 
-	arg = strtok(NULL, "\n\t\r "); /* get the argument after push */
-	if (arg == NULL) /* check if argument is present */
-	{
-		fprintf(stderr, "L%d: usage: push integer\n", counter);
-		exit(EXIT_FAILURE);
-	}
-	if (arg[0] == '-') /* handle negative sign */
-		j++;
-	for (; arg[j] != '\0'; j++) /* check if argument is valid */
-	{
-		if (arg[j] > 57 || arg[j] < 48)
-			flag = 1;
-	}
-	if (flag == 1) /* print error and exit if argument is invalid */
-	{
-		fprintf(stderr, "L%d: usage: push integer\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*stack);
-		exit(EXIT_FAILURE);
-	}
-	n = atoi(arg); /* convert argument to integer */
-	if (*stack == NULL) /* create new node and add it to stack or queue */
-	{
-		*stack = create_node(n);
-	}
-	else
-	{
-		if (bus.lifi == 0) /* add node to stack */
-			add_node(stack, n);
-		else /* add node to queue */
-			addqueue(stack, n);
-	}
+    if (global_state.arg == NULL)
+    {
+        fprintf(stderr, "L%d: usage: push integer\n", counter);
+        exit(EXIT_FAILURE);
+    }
+
+    /* Convert the string argument to an integer */
+    n = strtol(global_state.arg, &endptr, 10);
+
+    /* Check if the conversion was successful */
+    if (*endptr != '\0')
+    {
+        fprintf(stderr, "L%d: usage: push integer\n", counter);
+        exit(EXIT_FAILURE);
+    }
+
+    /* Add a node with the integer value to the stack or queue */
+    if (global_state.mode == STACK)
+    {
+        add_node(head, n);
+    }
+    else
+    {
+        add_queue(head, n);
+    }
 }
